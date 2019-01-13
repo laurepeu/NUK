6e9fa61b571e361f8a32d3b53da5ad27fec22c1b
laurepeu <laurepeucelle@MacBook-Pro-de-laure.local>
2019-01-13 22:35:31 +0100

    user to id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }
+    has_many :articles
 end
